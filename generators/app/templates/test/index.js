2fd70d67fa61ec6c7a5ed1c19fbbaed20590ab66
Eric McCormick <edm00se@gmail.com>
2017-04-21 21:13:16 -0500

    chore(app/test): updates template test for dynamic server config

--- generators/app/templates/test/index.js
+++ generators/app/templates/test/index.js
@@ -1,39 +1,32 @@
 'use strict';
 
-var fs = require('fs');
 var path = require('path');
-var child = require('child_process');
-
 var chai = require('chai');
 var chaiFiles = require('chai-files');
 var chaiHttp = require('chai-http');
 chai.use(chaiFiles);
 chai.use(chaiHttp);
-
 var expect = chai.expect;
 var file = chaiFiles.file;
 var dir = chaiFiles.dir;
 
-describe('app', function(){
+const server = require('../app');
 
-  describe('static presentation', function(){
-    it('has a generated an html file in docs/', function(){
+describe('app', () => {
+  describe('static presentation', () => {
+    it('has a generated an html file in docs/', () => {
       expect(dir('docs')).to.exist;
       expect(file(path.join(__dirname, '../docs/index.html'))).to.exist;
     });
   });
 
-  describe('server presentation', function(done){
-    it('should serve successfully', function(){
-      chai.request('http://localhost:8080')
-        .get('/')
-        .end(function (err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          done();
-        });
+  describe('server presentation', done => {
+    it('should serve successfully', () => {
+      chai.request(server).get('/').end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
     });
-
   });
-
 });
