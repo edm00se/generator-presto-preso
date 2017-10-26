37d87da8a79228acae543d99d83addca64926198
Eric McCormick <edm00se@gmail.com>
2017-10-25 21:59:35 -0500

    fix(templ): template static build fixed
    the static build required data.json is now in appropriate place, along with upgraded template deps,
    updated readme
    
    closes #32, closes #33, closes #34

--- generators/app/templates/routes/index.js
+++ generators/app/templates/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('http-auth');
-const aUnm = process.env.CONTROLLER_NAME || 'admin';
-const aPw = process.env.CONTROLLER_PW || 'someAmazingP@$$w0rd';
+const aUnm = process.env.PRESO_CONTROLLER_NAME || 'admin';
+const aPw = process.env.PRESO_CONTROLLER_PW || 'someAmazingP@$$w0rd';
 
 // Configure basic auth
 var basic = auth.basic({
